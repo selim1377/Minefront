f46d130fdbd6481156e3ee80bd55a2e955395ce3
Eivind <ela4991@gmail.com>
2012-04-19 17:23:39 +0200

    Changed applet name

---
+++ Minefront/src/com/mime/minefront/MinefrontApplet.java
@@ -0,0 +1,24 @@
+package com.mime.minefront;
+
+import java.applet.Applet;
+import java.awt.BorderLayout;
+
+public class MinefrontApplet extends Applet{
+
+	private static final long serialVersionUID = 1L;
+	private Display game = new Display();
+	
+	public void init() {
+		setLayout(new BorderLayout());
+		add(game);
+	}
+	
+	public void start() {
+		game.start();
+	}
+	
+	public void stop() {
+		game.stop();
+	}
+	
+}
